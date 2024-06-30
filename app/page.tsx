d72ae9db5268e7202a2becfb8c51c5dc7cf88ee9
Seokwoodang <swoo1429@naver.com>
2024-07-01 00:56:37 +0900

    edit : client component화  및 client component 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
-import AuthForm from "@/components/auth-form";
-import Image from "next/image";
+"use client";
+
+import {
+  TestDataType,
+  getTestData,
+  insertTestData,
+} from "@/action/test-action";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [test, setTest] = useState<TestDataType[] | null>();
+
+  useEffect(() => {
+    getTestData()
+      .then((data) => {
+        setTest(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <AuthForm />
+        {test?.map((value) => {
+          return <div key={value.id}>{value.id}</div>;
+        })}
       </div>
+      <button
+        onClick={() => {
+          insertTestData({ hello: false, testTwo: "hello" });
+        }}
+      >
+        hehe
+      </button>
     </main>
   );
 }

39ff1261e0c988764f11fd8c542c285ed92f34d2
Seokwoodang <swoo1429@naver.com>
2024-07-25 11:41:39 +0900

    remove : chatting restart

--- app/chatting/page.tsx
+++ app/chatting/page.tsx
@@ -2,22 +2,6 @@ import supabase from "@/supabase/supabaseClient";
 import { useState } from "react";
 
 export default function ChattingPage() {
-  const [step, setStep] = useState(1);
-  // Create a function to handle inserts
-  const handleInserts = () => {
-    console.log("Change received!");
-  };
-
-  // Listen to inserts
-  // supabase
-  //   .channel('CHAT-DATA')
-  //   .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'todos' }, handleInserts)
-  //   .subscribe()
-
-  const channelOn = () => {
-    // supabase.channel('room1').on('')
-  };
-
   return (
     <div>
       <div>채팅페이지</div>
